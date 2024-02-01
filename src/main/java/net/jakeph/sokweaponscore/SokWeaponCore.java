1b435459fa0894c8465ccbfcc777736c1a4ef24f
jakeph12 <savrasovvitya@gmail.com>
2024-02-01 21:40:13 +0100

    Initial commit

---
+++ src/main/java/net/jakeph/sokweaponscore/SokWeaponCore.java
@@ -0,0 +1,47 @@
+package net.jakeph.sokweaponscore;
+
+import com.mojang.logging.LogUtils;
+import net.jakeph.sokweaponscore.item.ModItems;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(SokWeaponCore.MOD_ID)
+public class SokWeaponCore
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "sokweaponscore";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public SokWeaponCore()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
---
+++ src/main/java/net/jakeph/sokweaponscore/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.jakeph.sokweaponscore.item;
+
+import net.jakeph.sokweaponscore.SokWeaponCore;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, SokWeaponCore.MOD_ID);
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+    public static final RegistryObject<Item> SwordForAkim = ITEMS.register("swordakim",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+}
